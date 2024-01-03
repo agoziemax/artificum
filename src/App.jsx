a5a4c980e5b09778782d093b8b9c5c7eb795edd4
Agoziemax <agoziemax@yahoo.com>
2024-01-03 15:56:59 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-
-
+import Button from '@mui/material/Button';
+import './index.css';
 function App() {
-
 
   return (
     <>
+      <div>
+        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <Button style={{ backgroundColor: 'red' }}>Click me</Button>
 
+        <button className='bg-red-500'>Click me</button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
